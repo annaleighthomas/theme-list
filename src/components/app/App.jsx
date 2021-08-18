b80762b96ae108cdcbd6f8011c32f8c630dac2d3
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-17 22:29:14 -0400

    returning header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CharacterList from './characters/CharacterList';
+import Header from '../../header/Header';
 
 
 export default function App() {
   return (
     <>
+      <Header />
       <CharacterList />
     </>
   );
